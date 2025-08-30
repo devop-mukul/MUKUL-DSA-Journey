fe0941987bcb3346f5ba65f169e6d15c7495a656
Mukul <mails2mukull@gmail.com>
2025-08-30 20:37:15 +0530

    FindSecond Largest Optimal Solution

--- Arrays/FindSecondLargest.java
+++ Arrays/FindSecondLargest.java
@@ -3,7 +3,7 @@ package Arrays;
 public class FindSecondLargest {
     public static void main(String[] args) {
         int[] arr = {12, 35, 1, 10, 34, 1};
-        System.out.println("The second largest element is: " + findSecondLargest(arr.length, arr));
+        System.out.println("The second largest element is: " + findSecondLargest2ndSol(arr.length, arr));
     }
     public static int findSecondLargest(int n, int[] arr) {
 		for(int i = 0; i < n - 1; i++) {
@@ -26,5 +26,20 @@ public class FindSecondLargest {
 			}
 		}
 		return -1;
+	}
+	public static int findSecondLargest2ndSol(int n, int []arr) {
+		int first = Integer.MIN_VALUE;
+		int second = Integer.MIN_VALUE;
+
+		for(int i = 0; i < n; i++) {
+			if(arr[i] > first){
+				second = first;
+				first = arr[i];
+			}
+			else if(arr[i] < first && arr[i] > second) {
+				second = arr[i];
+			}
+		}
+		return (second == Integer.MIN_VALUE) ? -1 : second;
 	}
 }
