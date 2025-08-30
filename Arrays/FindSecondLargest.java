0ae2669f6a586be54b0f3eb3c9cd4bd7ea6aff7a
Mukul <mails2mukull@gmail.com>
2025-08-30 22:43:33 +0530

    Rotatearray by k left and right

--- Arrays/FindSecondLargest.java
+++ Arrays/FindSecondLargest.java
@@ -3,7 +3,9 @@ package Arrays;
 public class FindSecondLargest {
     public static void main(String[] args) {
         int[] arr = {12, 35, 1, 10, 34, 1};
+	//               1,  1,  10, 12, 34, 35
         System.out.println("The second largest element is: " + findSecondLargest2ndSol(arr.length, arr));
+		System.out.println("The second smallest element is: " + findSecondSmallest2ndSol(arr.length, arr));
     }
     public static int findSecondLargest(int n, int[] arr) {
 		for(int i = 0; i < n - 1; i++) {
@@ -41,5 +43,20 @@ public class FindSecondLargest {
 			}
 		}
 		return (second == Integer.MIN_VALUE) ? -1 : second;
+	}
+
+	public static int findSecondSmallest2ndSol(int n, int arr[]) {
+		int first_smallest = Integer.MAX_VALUE;
+		int second_smallest = Integer.MAX_VALUE;
+
+		for(int i = 0; i < n; i++) {
+			if (arr[i] < first_smallest) {
+				second_smallest = first_smallest;
+				first_smallest = arr[i];
+			}
+			else if(arr[i] > first_smallest && arr[i] < second_smallest)
+				second_smallest = arr[i];
+		}
+		return (second_smallest == Integer.MAX_VALUE) ? -1 : second_smallest;
 	}
 }
