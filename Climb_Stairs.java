a615a80a9acf9a7f8d46433e5b9a7d746b3cf20a
Mukul <mails2mukull@gmail.com>
2025-09-02 01:00:08 +0530

    ClimbStairs

---
+++ Climb_Stairs.java
@@ -0,0 +1,22 @@
+
+
+public class Climb_Stairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(8));
+    }
+    public static int climbStairs(int n) {
+        if (n <= 3) return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+        int cur = 0;
+
+        for (int i = 3; i < n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;        
+    }
+}
