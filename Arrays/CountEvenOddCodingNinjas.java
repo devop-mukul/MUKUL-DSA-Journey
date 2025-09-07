c406503fe5512b7e40a567912d2e889ce81ee3c4
Mukul <mails2mukull@gmail.com>
2025-09-07 23:13:29 +0530

    CountEvenOddFreq

--- Arrays/CountEvenOddCodingNinjas.java
+++ Arrays/CountEvenOddCodingNinjas.java
@@ -4,23 +4,26 @@ import java.util.Arrays;
 
 public class CountEvenOddCodingNinjas {
     public static void main(String[] args) {
-        int arr[] = {6, 5, 3, 2, 2};
+        // int arr[] = {6, 5, 3, 2, 2};
+        int arr[] = {2, 1, 2, 1, 5, 5, 2};
         System.out.println(Arrays.toString(countEvenOdd(arr,arr.length)));
     }
     public static int[] countEvenOdd(int[] arr, int n) {
-        int evenCount = 0, oddCount = 0;
-        Arrays.sort(arr);  // sort so duplicates are adjacent
+        Arrays.sort(arr);
+        int evenCount = 0, oddCount = 0, freq = 1;
+        for(int i = 1; i < n; i++) {
+            if(arr[i] == arr[i - 1]) {
+                freq++;
+            }
+            else {
+                if(freq % 2 == 0)   evenCount++;
+                else    oddCount++;
 
-        for (int i = 0; i < n; i++) {
-            // count only if it's the first element OR different from the previous one
-            if (i == 0 || arr[i] != arr[i - 1]) {
-                if (arr[i] % 2 == 0)
-                    evenCount++;
-                else
-                    oddCount++;
+                freq = 1;
             }
         }
-
+        if(freq % 2 == 0)   evenCount++;
+        else    oddCount++;
         return new int[]{oddCount, evenCount};
     }
 }
--- Arrays/runningSum.java
+++ Arrays/runningSum.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class runningSum {
     public static void main(String[] args) {
         int nums[] = {3, 1, 2, 10, 1};
-        System.out.println(Arrays.toString(runningSum(nums)));
+        System.out.println(Arrays.toString(PrefixSum(nums)));
     }
-    public static int[] runningSum(int nums[]) {
+    public static int[] PrefixSum(int nums[]) {
         int runningSum = 0;
         for(int i = 0; i < nums.length; i++) {
             runningSum += nums[i];
