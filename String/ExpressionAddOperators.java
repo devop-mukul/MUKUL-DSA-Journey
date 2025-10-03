2484198be9983c5f30fb52c7701faa02e05f121a
Mukul <mails2mukull@gmail.com>
2025-10-03 12:58:22 +0530

    ExpressionAndOperators

---
+++ String/ExpressionAddOperators.java
@@ -0,0 +1,46 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionAddOperators {
+    public List<String> addOperators(String num, int target) {
+        List<String> rst = new ArrayList<String>();
+        if(num == null || num.length() == 0) return rst;
+        helper(rst, "", num, target, 0, 0, 0);
+        return rst;
+    }
+    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+        if(pos == num.length()){
+            if(target == eval)
+                rst.add(path);
+            return;
+        }
+        for(int i = pos; i < num.length(); i++){
+            if(i != pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if(pos == 0){
+                helper(rst, path + cur, num, target, i + 1, cur, cur);
+            }
+            else{
+                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
+                
+                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
+                
+                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ExpressionAddOperators solver = new ExpressionAddOperators();
+        String num = "123";
+        int target = 6;
+        List<String> results = solver.addOperators(num, target);
+
+        System.out.println("Expressions for num = \"" + num + "\" and target = " + target + ":");
+        for (String expr : results) {
+            System.out.println(expr);
+        }
+    }
+}
