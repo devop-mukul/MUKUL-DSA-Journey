0ba1a6bfbb6302e45004c8f3e378170c2a90cbfa
Mukul <mails2mukull@gmail.com>
2025-11-09 17:19:47 +0530

    return the sum as a linked list

---
+++ LinkedList/addTwoNumbers.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class addTwoNumbers {
+    public static void main(String[] args) {
+        // Create first linked list: 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // Create second linked list: 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        addTwoNumbers solution = new addTwoNumbers();
+        ListNode result = solution.addTwoNumbers1(l1, l2);
+
+        // Print result
+        System.out.print("Result: ");
+        while (result != null) {
+            System.out.print(result.val + " -> ");
+            result = result.next;
+        }
+        System.out.println("null");
+    }
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode res = dummy;
+        int total = 0, carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            total = carry;
+
+            if (l1 != null) {
+                total += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                total += l2.val;
+                l2 = l2.next;
+            }
+
+            int num = total % 10;
+            carry = total / 10;
+            dummy.next = new ListNode(num);
+            dummy = dummy.next;
+        }
+
+        return res.next;        
+    }
+}
